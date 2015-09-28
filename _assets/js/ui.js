213d8a903491e8162e8302a4ad729531f171d356
Russell Matsuo <design@russellmatsuo.com>
2015-09-27 20:50:45 -0700

    added idle tooltip interaction

--- _assets/js/ui.js
+++ _assets/js/ui.js
@@ -2,6 +2,7 @@
 	var canvas = document.getElementById("solar_system"),
 		cursor = document.getElementById("cursor"),
 		caption = document.getElementById("caption"),
+		clicked_once = false,
 
 		//Timers
 		debounce_resize,
@@ -27,16 +28,65 @@
 		var cx = e.clientX - (cursor.offsetWidth / 2),
 			cy = e.clientY - (cursor.offsetHeight / 2);
 		cursor.style.transform = "translate(" + cx + "px, " + cy + "px)";
+		caption.style.transform = "translate(" + cx + "px, " + cy + "px)";
 	},
 
 	position_caption = function(){
 		query_window_dimensions();
 		caption.style.bottom = window_h / 2 + "px";
+	},
+
+	init_tooltip = function(){
+		TweenMax.set("#caption-ink", {
+			scale: 0
+		});
+
+		TweenMax.set("#caption-message", {
+			autoAlpha: 0
+		});
+	},
+
+	close_tooltip = function(){
+		TweenMax.to("#caption-message", .12, {
+			autoAlpha: 0,
+		});
+		TweenMax.to("#caption", .212, {
+			boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)",
+			marginTop: "0"
+		});
+		TweenMax.to("#caption-ink", .12, {
+			scale: 0,
+			delay: .1
+		});
+	},
+
+	open_tooltip = function(){
+		TweenMax.to("#caption-ink", .212, {
+			scale: 1,
+		});
+		TweenMax.to("#caption", .212, {
+			boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.25)",
+			marginTop: "-3px"
+		});
+		TweenMax.to("#caption-message", .212, {
+			autoAlpha: 1,
+			delay: .1
+		});
+	},
+
+	set_idle_timer = function(){
+		idle_timer = setTimeout(function() {
+			if(!clicked_once)
+			open_tooltip();
+		}, 2000);
 	};
 
 	//init
 	size_canvas();
 	position_caption();
+	set_idle_timer();
+	init_tooltip();
+
 
 	//Events
 	window.onresize = function(e){
@@ -48,10 +98,17 @@
 	}
 
 	window.onmousemove = function(e){
+		cursor.style.visibility = "visible";
 		position_cursor(e);
+
+		close_tooltip();
+		clearTimeout(idle_timer);
+		set_idle_timer();
 	}
 
 	canvas.onmousedown = function(e){
+		close_tooltip();
+		clicked_once = true;
 		TweenMax.to("#header", .412, {
 			height: 10
 		})
@@ -71,10 +128,6 @@
 				onComplete: function(){
 					cursor.className = "vector"
 				}
-			})
-			TweenMax.to("#caption", .412, {
-				opacity: 0,
-				scale: .95
 			})
 			clicked = true;
 		}
